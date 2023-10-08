0fdfd594c55bd8bc4d4ca682add333e3fca0962d
SyoInoue <funnyfacee7777@gmail.com>
2023-10-08 21:48:20 +0900

    getメソッドにif文を追加

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Getメソッドでリクエストを受け取った時の処理
 router.get("/", (req, res) => {
-  res.send({ message: "Hello" });
+  res.setHeader("X-Timestamp", Date.now());
+  let message = req.query.message;
+  const lang = req.headers["x-lang"];
+
+  if (message === "") {
+    res.status(400);
+    if (lang === "en") {
+      message = "message is empty.";
+    } else {
+      message = "messageの値が空です";
+    }
+  }
+  res.send({ message });
 });
 
 // ブラウザからjsonデータを受け取るための設定
