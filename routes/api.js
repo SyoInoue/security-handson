aaefba004055123533640406df2ab37607a78eb6
SyoInoue <funnyfacee7777@gmail.com>
2023-10-14 22:16:27 +0900

    CORS設定を追記

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+// ブラウザからjsonデータを受け取るための設定
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "X-Token");
+  }
+  next();
+});
+
 // Getメソッドでリクエストを受け取った時の処理
 router.get("/", (req, res) => {
   res.setHeader("X-Timestamp", Date.now());
   let message = req.query.message;
   const lang = req.headers["x-lang"];
-
   if (message === "") {
     res.status(400);
     if (lang === "en") {
@@ -18,8 +26,6 @@ router.get("/", (req, res) => {
   res.send({ message });
 });
 
-// ブラウザからjsonデータを受け取るための設定
-router.use(express.json());
 router.post("/", (req, res) => {
   const body = req.body;
   console.log(body);
