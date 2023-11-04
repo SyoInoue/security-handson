f025914603bc431bc488554fb040273987304b57
SyoInoue <funnyfacee7777@gmail.com>
2023-11-04 17:36:55 +0900

    CSRF対策

--- routes/csrf.js
+++ routes/csrf.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
 const router = express.Router();
 
 /**
@@ -28,12 +29,25 @@ router.post("/login", (req, res) => {
     res.send("ログインに失敗しました");
     return;
   }
-
   // セッションにユーザ名を保存
   sessionData = req.session;
   sessionData.username = username;
+  const token = crypto.randomUUID();
+  res.cookie("csrf_token", token, { secure: true });
   // CSRF検証用ページへリダイレクト
   res.redirect("/csrf_test.html");
+});
+
+router.post("/remit", (req, res) => {
+  // セッションに保存した情報からログイン済みかを確認する
+  if (!req.session.username || req.session.username !== sessionData.username) {
+    res.status(403);
+    res.send("ログインしていません");
+    return;
+  }
+  // 本来はデータベースの書き換えなど重要な処理が行われる
+  const { to, amount } = req.body;
+  res.send(`${to}に${amount}円送金しました`);
 });
 
 module.exports = router;
